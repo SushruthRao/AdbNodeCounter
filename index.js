5488c5d8aa7dc3f689c0f443ee144025363b087d
SushruthRao <90977964+SushruthRao@users.noreply.github.com>
2024-10-04 15:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+require('dotenv').config(); // Load environment variables from .env
 
+// Initialize Firebase Admin with service account key from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://instacount-84df9-default-rtdb.asia-southeast1.firebasedatabase.app'
